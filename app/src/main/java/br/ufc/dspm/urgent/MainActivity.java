5e48c6ad26840313384030d8eb10ef405d6d72d6
Gustavo Belo Brilhante <Gustavo@MacBook-Air-de-Gustavo.local>
2016-06-29 21:08:33 -0300

    Click do botão postos de saude

--- app/src/main/java/br/ufc/dspm/urgent/MainActivity.java
+++ app/src/main/java/br/ufc/dspm/urgent/MainActivity.java
@@ -4,16 +4,16 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ViewFlipper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -53,6 +53,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void visualizarPostos(View view){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+        } else {
+            mapsPostoDeSaude();
+        }
+    }
+
     public void visualizarUpas(View view) {
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
@@ -64,6 +76,30 @@ public class MainActivity extends AppCompatActivity {
         } else {
             mapsUpa();
         }
+
+    }
+
+    private void mapsPostoDeSaude(){
+        ArrayList<PostoDeSaude> enderecoList = Util.getEnderecoList(this);
+        ArrayList<PostoDeSaude> postos = Util.setCoordinatesByAddress(enderecoList, this);
+
+        double[] latlngs = new double[postos.size() * 2];
+        Bundle bundle = new Bundle();
+
+        for (int i = 0, j = 0; i < latlngs.length; i += 2, j++) {
+
+            latlngs[i] = postos.get(j).getLatitude();
+            latlngs[i + 1] = postos.get(j).getLongitude();
+
+        }
+
+        bundle.putDoubleArray("Localizations", latlngs);
+
+        Intent intent = new Intent(this, MapsActivity.class);
+        intent.putExtras(bundle);
+
+        startActivity(intent);
+
 
     }
 
